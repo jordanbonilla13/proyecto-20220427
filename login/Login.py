bc3ac55c5e50a77884a5c9cb6d4e82ca46f45f5f
elAtravesadorDeLaMancha <2222934@alu.murciaeduca.es>
2022-04-27 08:57:14 +0200

    Caracteristica terminada

---
+++ login/Login.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Login:
+    user: str
+    password: str
---
+++ login/test.py
@@ -0,0 +1,6 @@
+from Login import Login
+
+
+login = Login('foo', 'bar')
+print(f'Usuario: {login.user}')
+print(f'Contraseña: {login.password}')
